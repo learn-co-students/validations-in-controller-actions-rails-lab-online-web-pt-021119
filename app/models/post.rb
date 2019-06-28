6fbcdfa93e4737daca5b4a0665d226f7b25622e3
luizfper <luizfper@gmail.com>
2019-06-28 16:55:07 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+
+	validates :content, length: {minimum: 100}
+	validates :title, presence: true
+	validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
 end
