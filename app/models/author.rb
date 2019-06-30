c80fb8396dfb89d4e0bad77e2d9a2ee6701fe256
github username <github email address>
2019-06-30 14:16:44 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   #has_many :posts
-  validate :name, presence :true
-  validate :email, uniqueness :true
+  validates :name, presence: true
+  validates :email, uniqueness: true
 end
